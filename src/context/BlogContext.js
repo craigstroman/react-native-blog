3ede18982b604e1c0d9bd3b6e6d1e53f26bff3dc
Craig Stroman <cstroman@gmail.com>
2023-11-08 22:47:28 -0500

    Deleting a blog post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,7 +4,15 @@ import createDataContext from './createDataContext';
 const blogReducer = (state, action) => {
   switch (action.type) {
     case 'add_blogPost':
-      return [...state, { title: `Blog Post #${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 9999),
+          title: `Blog Post #${state.length + 1}`,
+        },
+      ];
+    case 'delete_blogPost':
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     default:
       return state;
   }
@@ -16,8 +24,14 @@ const addBlogPost = (dispatch) => {
   };
 };
 
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: 'delete_blogPost', payload: id });
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 );
